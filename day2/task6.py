fe4e5d826659debb7e02a5224b6eda9aeb63e2f4
Aliaksandr Dakutovich <alex.doka@gmail.com>
2019-10-28 23:11:46 +0300

    python day2 v.5

--- day2/task6.py
+++ day2/task6.py
@@ -2,6 +2,6 @@
 str = input("Input some sentense: ")
 x = str.split()
 # ./day2/task6.py:4:11: E701 multiple statements on one line (colon) if i do:
-#for i in x: print(i[::-1], end=" ") so in 2 lines:
+# for i in x: print(i[::-1], end=" ") so in 2 lines:
 for i in x:
     print(i[::-1], end=" ")
